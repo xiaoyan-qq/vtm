94f270e4de1910e323ea84c2d9054cb41c8cd2a3
xiaoyan-qq-home <xiaoyan159@qq.com>
2018-03-22 01:01:09 +0800

    增加权限申请的代码

--- cateye-vtm/src/main/java/com/cateye/android/vtm/MainActivity.java
+++ cateye-vtm/src/main/java/com/cateye/android/vtm/MainActivity.java
@@ -1,18 +1,28 @@
 package com.cateye.android.vtm;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.cateye.vtm.fragment.CatEyeMainFragment;
 import com.jkb.fragment.rigger.annotation.Puppet;
 import com.jkb.fragment.rigger.rigger.Rigger;
+import com.vondear.rxtools.view.dialog.RxDialog;
+import com.vondear.rxtools.view.dialog.RxDialogSure;
+import com.yanzhenjie.permission.Action;
+import com.yanzhenjie.permission.AndPermission;
+import com.yanzhenjie.permission.Permission;
+import com.yanzhenjie.permission.Rationale;
+import com.yanzhenjie.permission.RequestExecutor;
 
 import org.oscim.android.filepicker.FilePicker;
 import org.oscim.android.filepicker.FilterByFileExtension;
 import org.oscim.android.filepicker.ValidMapFile;
 import org.oscim.android.filepicker.ValidRenderTheme;
 
-@Puppet(containerViewId = R.id.fragment_main_container,bondContainerView = true)
+import java.util.List;
+
+@Puppet(containerViewId = R.id.fragment_main_container, bondContainerView = true)
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -22,11 +32,32 @@ public class MainActivity extends AppCompatActivity {
 
         //启动fragment，显示地图界面
         Rigger.getRigger(this).startFragment(CatEyeMainFragment.newInstance(new Bundle()));
+        //申请所需要的权限
+        AndPermission.with(this).permission(Permission.Group.LOCATION/*定位权限*/, Permission.Group.STORAGE/*存储权限*/, Permission.Group.SENSORS/*传感器*/)
+                .onGranted(new Action() {//用户允许
+                    @Override
+                    public void onAction(List<String> permissions) {
+
+                    }
+                })
+                .onDenied(new Action() {//用户拒绝
+            @Override
+            public void onAction(List<String> permissions) {
+                if (AndPermission.hasAlwaysDeniedPermission(MainActivity.this, permissions)) {
+                    // 这些权限被用户总是拒绝。
+                }
+            }
+        }).rationale(new Rationale() {
+            @Override
+            public void showRationale(Context context, List<String> permissions, RequestExecutor executor) {
+
+            }
+        }).start();
     }
 
     /**
      * 选择本地地图文件的文件选择过滤器
-     * */
+     */
     public static class MapFilePicker extends FilePicker {
         public MapFilePicker() {
             setFileDisplayFilter(new FilterByFileExtension(".map"));
@@ -36,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * 选择本地地图样式文件的文件选择过滤器
-     * */
+     */
     public static class ThemeFilePicker extends FilePicker {
         public ThemeFilePicker() {
             setFileDisplayFilter(new FilterByFileExtension(".xml"));
--- cateye-vtm/src/main/java/com/cateye/vtm/fragment/DrawPointLinePolygonFragment.java
+++ cateye-vtm/src/main/java/com/cateye/vtm/fragment/DrawPointLinePolygonFragment.java
@@ -2,16 +2,25 @@ package com.cateye.vtm.fragment;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import com.cateye.android.vtm.R;
 import com.jkb.fragment.rigger.annotation.Puppet;
 import com.jkb.fragment.rigger.rigger.Rigger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by xiaoxiao on 2018/3/21.
  */
 @Puppet(bondContainerView = false)
 public class DrawPointLinePolygonFragment extends BaseFragment {
+    private CheckBox chk_draw_point, chk_draw_line, chk_draw_polygon;
+    private List<CheckBox> checkBoxes;
+    private DRAW_STATE currentDrawState = DRAW_STATE.DRAW_NONE;
+
     @Override
     public int getFragmentLayoutId() {
         return R.layout.fragment_draw_point_line_polygon;
@@ -19,7 +28,60 @@ public class DrawPointLinePolygonFragment extends BaseFragment {
 
     @Override
     public void initView(View rootView) {
+        chk_draw_point = rootView.findViewById(R.id.chk_draw_point);
+        chk_draw_line = rootView.findViewById(R.id.chk_draw_line);
+        chk_draw_polygon = rootView.findViewById(R.id.chk_draw_polygon);
+        checkBoxes=new ArrayList<>();
+        checkBoxes.add(chk_draw_point);
+        checkBoxes.add(chk_draw_line);
+        checkBoxes.add(chk_draw_polygon);
 
+
+        chk_draw_point.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //将其他两个控件的选中状态置为未选中
+                    if (checkBoxes != null && !checkBoxes.isEmpty()) {
+                        for (CheckBox chk : checkBoxes) {
+                            if (chk != chk_draw_point) {
+                                chk.setChecked(false);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        chk_draw_line.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //将其他两个控件的选中状态置为未选中
+                    if (checkBoxes != null && !checkBoxes.isEmpty()) {
+                        for (CheckBox chk : checkBoxes) {
+                            if (chk != chk_draw_line) {
+                                chk.setChecked(false);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        chk_draw_polygon.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    //将其他两个控件的选中状态置为未选中
+                    if (checkBoxes != null && !checkBoxes.isEmpty()) {
+                        for (CheckBox chk : checkBoxes) {
+                            if (chk != chk_draw_polygon) {
+                                chk.setChecked(false);
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public static BaseFragment newInstance(Bundle bundle) {
@@ -31,5 +93,24 @@ public class DrawPointLinePolygonFragment extends BaseFragment {
     @Override
     public void onRiggerBackPressed() {
         Rigger.getRigger(this).close();
+    }
+
+    public DRAW_STATE getCurrentDrawState() {
+        if (checkBoxes != null && !checkBoxes.isEmpty()) {
+            for (CheckBox chk : checkBoxes) {
+                if (chk == chk_draw_point) {
+                    return DRAW_STATE.DRAW_POINT;
+                } else if (chk == chk_draw_line) {
+                    return DRAW_STATE.DRAW_LINE;
+                } else if (chk == chk_draw_polygon) {
+                    return DRAW_STATE.DRAW_POLYGON;
+                }
+            }
+        }
+        return DRAW_STATE.DRAW_NONE;
+    }
+
+    public enum DRAW_STATE {
+        DRAW_NONE, DRAW_POINT, DRAW_LINE, DRAW_POLYGON
     }
 }
